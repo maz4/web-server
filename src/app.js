9377e66863157d640aae7af9d841399cd7fa7eac
Marcin <zasadzkimarcin@gmail.com>
2020-02-21 08:07:18 +0000

    add 404 page and simple styles

--- src/app.js
+++ src/app.js
@@ -47,6 +47,22 @@ app.get('/weather', (req, res) => {
   });
 });
 
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Marcin',
+    errorMessage: 'Help article not found',
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Marcin',
+    errorMessage: '404 Page not found',
+  })
+})
+
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
 });
