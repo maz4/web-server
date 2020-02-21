00810fcefb7f90c5801dba32043f2594c5de6e58
Marcin <zasadzkimarcin@gmail.com>
2020-02-21 07:25:39 +0000

    add templating with hbs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,52 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectory = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDirectory = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectory));
+
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Marcin'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: "About Me",
+    name: "Marcin"
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'This is help message',
+    title: 'Help',
+    name: 'Marcin'
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'It is snowing',
     location: 'New York'
   });
-})
+});
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
-})
+});
